d06c20a0aeb0f0a56fac235e017582aa6e3c5032
dchang74 <danielc718@gmail.com>
2020-11-23 19:06:46 -0500

    firebase-auth-demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import {AuthProvider} from "./context/AuthContext"
+import Home from "./views/Home"
+import Login from "./views/Login"
+import SignUp from "./views/Signup"
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <Router>
+          <div>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            {/* <h1>Hello</h1> */}
+          </div>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
